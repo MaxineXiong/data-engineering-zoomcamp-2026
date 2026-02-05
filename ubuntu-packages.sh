a7c8546f633e7f34d14b50c19ef1c5208879bab8
Maxine Xiong <55864839+MaxineXiong@users.noreply.github.com>
2026-02-05 12:20:03 +0000

    Config files for installing Terraform

---
+++ ubuntu-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##########################  Installing Terraform  #########################################
+sudo apt install  software-properties-common gnupg2 curl
+ curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
+sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform
+
+
+# Execute `sh ubuntu-packages.sh` in terminal to install Terraform in Codespace
